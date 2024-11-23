ed93d4c116e2c9029cd379f155f2a4c8851941fa
yurei <167486448+yurei0903@users.noreply.github.com>
2024-11-23 16:51:17 +0900

    update 4 files and create 2 files

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,11 +1,13 @@
-const index_button_index=document.getElementById('ind_index');
-index_button_index.addEventListener('click',function(){
-    const index_link_index=document.getElementById("index_link");
-    if(index_link_index.style.display==="block")
-        index_link_index.style.display="none";
+var index_button=document.getElementById('ind_index');
+const index_link=document.getElementById("index_link");
+index_button.addEventListener('click',function(){
+    if(index_link.style.display==="block")
+        index_link.style.display="none";
     else{
-        index_link_index.style.display="block";
+        index_link.style.display="block";
     }
-    consle.log("A");
 });
 
+index_link.addEventListener('blur', function(){
+    index_link.style.display="none";
+});
